a7afc96de45e3211aff97ef4326f3a41e56319da
2023JoshuaP <jphilco@unsa.edu.pe>
2026-06-05 12:44:49 -0500

    Structure to upload dataset.

---
+++ include/DataLoader.cuh
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Matrix.cuh"
+#include <vector>
+#include <string>
+#include <utility>
+#include <map>
+
+using namespace std;
+
+struct DataLoader {
+    static vector<string> CLASS_NAMES;
+    static map<int, int> SYMBOL_ID_TO_INDEX;
+    static int NUM_CLASSES;
+
+    static pair<Matrix, Matrix> load_data(const string& dir);
+    static pair<Matrix, Matrix> load_csv_data(const string& csv_path, const string& base_dir, int img_size = 32);
+
+    static void load_symbols(const string& symbols_csv);
+
+    static double accuracy(const Matrix& predictions, const Matrix& trues);
+};
